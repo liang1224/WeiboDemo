69f8c74e2f34cce542b8cf81d07a3eab5fa3b413
liangjw <liangjw1224@gmail.com>
2020-11-28 19:01:17 +0800

    Initial Commit

---
+++ weibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weibo
+//
+//  Created by 梁家伟 on 2020/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weibo/weiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  weiboApp.swift
+//  weibo
+//
+//  Created by 梁家伟 on 2020/11/28.
+//
+
+import SwiftUI
+
+@main
+struct weiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
